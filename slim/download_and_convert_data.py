8f53f2472f395a04be9be0a31555d0d48ad855bf
ThejanW <thejanwijesinghe.14@cse.mrt.ac.lk>
2017-06-22 17:03:02 +0530

    Add informative comments

--- slim/download_and_convert_data.py
+++ slim/download_and_convert_data.py
@@ -31,8 +31,8 @@ $ python download_and_convert_data.py \
     --dataset_dir=/tmp/flowers
 
 $ python download_and_convert_data.py \
-    --dataset_name=xmas \
-    --dataset_dir=/tmp/datasets/xmas
+    --dataset_name=custom \
+    --dataset_dir=/tmp/datasets/custom
 
 ```
 """
@@ -42,17 +42,14 @@ from __future__ import print_function
 
 import tensorflow as tf
 
-from datasets import download_and_convert_cifar10
-from datasets import download_and_convert_flowers
-from datasets import download_and_convert_mnist
-from datasets import download_and_convert_xmas
+from datasets import download_and_convert_custom
 
 FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_string(
     'dataset_name',
     None,
-    'The name of the dataset to convert, one of "cifar10", "flowers", "mnist", "xmas"')
+    'The name of the dataset to convert, one of "cifar10", "flowers", "mnist", "custom"')
 
 tf.app.flags.DEFINE_string(
     'dataset_dir',
@@ -66,14 +63,9 @@ def main(_):
     if not FLAGS.dataset_dir:
         raise ValueError('You must supply the dataset directory with --dataset_dir')
 
-    if FLAGS.dataset_name == 'cifar10':
-        download_and_convert_cifar10.run(FLAGS.dataset_dir)
-    elif FLAGS.dataset_name == 'flowers':
-        download_and_convert_flowers.run(FLAGS.dataset_dir)
-    elif FLAGS.dataset_name == 'mnist':
-        download_and_convert_mnist.run(FLAGS.dataset_dir)
-    elif FLAGS.dataset_name == 'xmas':
-        download_and_convert_xmas.run(FLAGS.dataset_dir)
+    # Add custom dataset_name
+    if FLAGS.dataset_name == 'custom':
+        download_and_convert_custom.run(FLAGS.dataset_dir)
     else:
         raise ValueError(
             'dataset_name [%s] was not recognized.' % FLAGS.dataset_dir)
